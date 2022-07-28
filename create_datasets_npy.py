0886f3ae29f3d08560f2363b41debaaa0d333aba
DanielHU <dan3455527@gmail.com>
2022-07-28 22:45:07 +0800

    init commit

---
+++ create_datasets_npy.py
@@ -0,0 +1,60 @@
+import os
+import torch
+import numpy as np
+import torch.nn.functional as F
+# data storage structure
+# test
+#   |--cats
+#   |--dogs
+# train
+#   |--cats
+#   |--dogs
+
+
+def data_loader_list():
+  data_ls = []
+  pass
+
+
+data_ls = []
+label_ls = []
+test_data_ls = []
+test_label_ls = []
+
+label_class = {"dog":0, "cat":1}
+# training set
+for root, dirs, files in os.walk("./data/training_set/training_set/cats"):
+  for file in files:
+    if ".jpg" in file:
+      data_ls.append(os.path.join(root, file))
+      label_ls.append(0)
+
+for root, dirs, files in os.walk("./data/training_set/training_set/dogs"):
+  for file in files:
+    if ".jpg" in file:
+      data_ls.append(os.path.join(root, file))
+      label_ls.append(1)
+
+# test set
+for root, dirs, files in os.walk("./data/test_set/test_set/cats"):
+  for file in files:
+    if ".jpg" in file:
+      test_data_ls.append(os.path.join(root, file))
+      test_label_ls.append(0)
+
+for root, dirs, files in os.walk("./data/test_set/test_set/dogs"):
+  for file in files:
+    if ".jpg" in file:
+      test_data_ls.append(os.path.join(root, file))
+      test_label_ls.append(1) 
+      
+def onehot(input_list):
+  tensor_ls = torch.tensor(input_list)
+  tensor_ls = F.one_hot(tensor_ls)
+  input_list = tensor_ls.tolist()
+  return input_list
+
+np.save("train_data_path.npy", data_ls)
+np.save("labels_onehot.npy", label_ls)
+np.save("test_data_path.npy", test_data_ls)
+np.save("test_labels_onehot.npy", test_label_ls)
---
+++ utils/utils.py
@@ -0,0 +1,102 @@
+import torch
+import datetime
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+
+
+
+def train(n_epochs, optimizer, model, device, loss_func, train_loader, verbose=True, validation_loader=None):
+  history = {"loss":[], "acc":[], "val_loss":[], "val_acc":[]}
+  model.to(device)
+  for epoch in range(1, n_epochs+1):
+    running_loss = 0.0
+    correct = 0.0
+    val_loss = 0.0
+    val_correct = 0.0
+    train_total = 0
+    val_total = 0
+    
+    # training part
+    model.train()
+    for batch_idx, (input, labels) in enumerate(train_loader):
+      input, labels = input.to(device), labels.to(device)
+      optimizer.zero_grad()
+      output = model(input)
+      loss =  loss_func(output, labels)
+      loss.backward()
+      optimizer.step()
+      running_loss += loss.item()
+
+
+    # validation part
+    if validation_loader != None:
+      model.eval()
+      for _batch_idx, (val_input, val_labels) in enumerate(validation_loader):
+        val_input, val_labels = val_input.to(device), val_labels.to(device)
+        val_output = model(val_input)
+        loss = loss_func(val_output, val_labels)
+        val_loss += loss.item()
+
+    if epoch == 1 or epoch % 1 == 0:
+      # training loss and accuracy calculation
+      pred = torch.max(output, dim=1)[1]
+      correct += (pred == labels).sum().item()
+      train_total += labels.size(0)
+      acc = correct / train_total
+      output_str = f"{datetime.datetime.now()},Epoch {epoch}\tTraining loss {running_loss / len(train_loader):.6f}, Acc {acc:.6f} "
+      history["loss"].append(running_loss/ len(train_loader.dataset))
+      history["acc"].append(acc)
+      # validation loss and accuracy calculation
+      if validation_loader != None:
+        val_pred = torch.max(val_output, dim=1)[1]
+        val_correct += (val_pred == val_labels).sum().item()
+        val_total += val_labels.size(0)
+        val_acc = val_correct / val_total
+        output_str += f"Val loss {val_loss / len(validation_loader):.6f}, val_acc {val_acc:.6f}"
+        history["val_loss"].append(val_loss/ len(validation_loader.dataset))
+        history["val_acc"].append(val_acc)
+      print(output_str, end="")
+      if verbose == False:
+        print("\r", end="", flush=True)
+      else:
+        print("\n")
+  return history
+
+def test(model, test_loader, device):
+  model.eval()
+  model.to(device)
+  test_correct = 0.0
+  test_total = 0
+  with torch.no_grad():
+    for _batch_id, (input, labels) in enumerate(test_loader):
+      input, labels = input.to(device), labels.to(device)
+      output = model(input)
+      pred = torch.max(output, dim=1)[1]
+      test_total += labels.size(0)
+      test_correct += (pred == labels).sum().item()
+      test_acc = test_correct / test_total
+    print(f"Accuracy: {test_acc:.6f}")
+  return test_acc
+
+def plot_curve(history_dict):
+  plt.plot(history_dict["loss"])
+  if history_dict["val_loss"]:
+    plt.plot(history_dict["val_loss"])
+  plt.title("model loss")
+  plt.ylabel("loss")
+  # plt.ylim(0,)
+  plt.xlabel("epochs")
+  plt.legend(["train", "val"], loc="upper right")
+  plt.show()
+  
+  plt.plot(history_dict["acc"])
+  if history_dict["val_acc"]:
+    plt.plot(history_dict["val_acc"])
+  plt.title("model acc")
+  plt.ylabel("acc")
+  plt.ylim(0,)
+  plt.xlabel("epochs")
+  plt.legend(["train", "val"], loc="upper right")
+  plt.tight_layout()
+  plt.show() 
+  pass
